2c45ed6e9ac6b958520caa99f4b962c504844164
Andrew Chang <acchang4@gmail.com>
2021-01-06 22:04:12 -0500

    adding first set of html, js, css files

---
+++ script.js
@@ -0,0 +1,6 @@
+// I need to get every square to acknowledge if an X or O is clicked
+
+document.getElementById("0").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("0").innerHTML = Date();}
