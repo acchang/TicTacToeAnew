01fd1863d35eb8ce2ff3e1578748e9c896a1e95e
Andrew Chang <acchang4@gmail.com>
2021-01-14 09:57:52 -0500

    begin work on player choice

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const boxes = Array.from(document.getElementsByClassName('box'));
+// 1) CLEAN THIS UP SO BOTH SIDES CAN PLAY AND PLAYERS CAN BE PICKED
+// 2) REFACTOR TO CONSOLIDATED, NO NEED TO GENERATE INDEXSELECTED
+// need to work on indeterminate players; current player is 'one' or 'two'
 
-let currentPlayer = "playerOne"
-let board = []
+let currentPlayer = 'playerOne'
+    // const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    // const X_CLASS = 'x'
+    // const CIRCLE_CLASS = 'circle'
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
 
 const winningTrios = [
 	[0, 1, 2],
@@ -14,17 +22,18 @@ const winningTrios = [
 	[6, 4, 2]
 ]
 
+const boxes = Array.from(document.getElementsByClassName('box'));
+
 const gridOfBoxes = document.querySelectorAll('.box');
 for (const gridBox of gridOfBoxes) {
     gridBox.addEventListener('click', boxmarked,  {once: true})
 }
 
 function boxmarked(e) {
-    e.target.innerHTML = "icon goes here";
+    // e.target.innerHTML = "icon goes here";
     const gridArray = Array.from(gridOfBoxes)
     const index = gridArray.indexOf(e.target)
 
-    // need to work on indeterminate players; current player is 'one' or 'two'
     if(currentPlayer === 'playerOne') {
         gridArray[index].classList.add('X')
         currentPlayer = 'playerTwo'
@@ -36,13 +45,11 @@ function boxmarked(e) {
     console.log(e.target.classList[1], index, gridArray)
     
     checkWin()
-
-    // *** FCC does it by passing in board and player. I need the board for the minimax and I need the current player
-    // he .reduces the board to get an array of the occupied places
-    // then he also employs an .every and for loop (25:00 in video)
-    // NEXT STEPS: CLEAN THIS UP SO BOTH SIDES CAN PLAY AND PLAYERS CAN BE PICKED
-    // ALSO: MAYBE REFACTOR TO CONSOLIDATED, NO NEED TO GENERATE INDEXSELECTED
 }
+
+// *** FCC does it by passing in board and player. I need the board for the minimax and I need the current player
+// he .reduces the board to get an array of the occupied places
+// then he also employs an .every and for loop (25:00 in video)
 
 function checkWin() {
     var indexSelected = boxes.reduce((indexSelected, box, idx) => {
@@ -55,8 +62,11 @@ function checkWin() {
 
    var winner = winningTrios.some(trio => {
        return trio.every(i => indexSelected.includes(i))});
+
     if (winner === true) {alert ("game won")}
-   }
+}
+
+
 
 // try refactoring to do as below.
 // function checkWin(currentClass) {
