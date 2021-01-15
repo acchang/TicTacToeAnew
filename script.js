eb51fcf71117d09bd4c7f5550045c55b4529c117
Andrew Chang <acchang4@gmail.com>
2021-01-15 16:10:31 -0500

    both players work

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+// 1) CLEAN THIS UP SO BOTH SIDES CAN PLAY AND PLAYERS CAN BE PICKED
+// event listener to dropdown, equate to player choice, start game with player choice
+// 2) REFACTOR TO CONSOLIDATED, NO NEED TO GENERATE INDEXSELECTED
+//      ALLOW BOTH SIDES TO WIN
+// 3) DUMB AI
+// 4) SMART AI
+// need to work on indeterminate players; current player is 'one' or 'two'
 
 let playerTurn
 
 const ECKS_CLASS = 'X'
 const OH_CLASS = 'O'
-const currentClass = playerTurn ? ECKS_CLASS : OH_CLASS
+// const currentClass = playerTurn ? ECKS_CLASS : OH_CLASS
 
-// const currentClass = if(playerTurn) {ECKS_CLASS} else {OH_CLASS};
+function checkClass() {
+  if(playerTurn) {
+  currentClass = ECKS_CLASS
+} else {
+  currentClass = OH_CLASS
+};}
+
+
+function currentClass() {
+  if(playerTurn) {
+  return ECKS_CLASS
+} else {
+  return OH_CLASS
+};}
+
 
 function swapTurns() {
   playerTurn = !playerTurn
@@ -50,15 +71,21 @@ function boxmarked(e) {
         e.target.innerHTML = "X"
       }
 
-    console.log(currentClass, e.target.classList[1], index, arrayfromBoxes)
+    console.log(e.target.classList[1], index, arrayfromBoxes)
     swapTurns()
     
     checkWin()
 }
 
+// *** FCC does it by passing in board and player. I need the board for the minimax and I need the current player
+// he .reduces the board to get an array of the occupied places
+// then he also employs an .every and for loop (25:00 in video)
+
 function checkWin() {
+    checkClass();
+    console.log(currentClass)
     var indexSelected = arrayfromBoxes.reduce((indexSelected, box, idx) => {
-        if (box.classList[1] === "X") {
+        if (box.classList[1] === currentClass) {
             indexSelected.push(idx);
         }
         return indexSelected;
@@ -72,3 +99,9 @@ function checkWin() {
     // then remove all eventlisteners
 }
 
+// try refactoring to do as below.
+// function checkWin(currentClass) {
+//     return WINNING_COMBINATIONS.some(combination => {
+//       return combination.every(index => {
+//         return cellElements[index].classList.contains(currentClass)
+//       })
