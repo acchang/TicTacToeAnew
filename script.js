c490111730063952b105645f1c4a151bbc2f3fa9
Andrew Chang <acchang4@gmail.com>
2021-01-15 13:56:21 -0500

    switching players

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// 1) CLEAN THIS UP SO BOTH SIDES CAN PLAY AND PLAYERS CAN BE PICKED
-// event listener to dropdown, equate to player choice, start game with player choice
-// 2) REFACTOR TO CONSOLIDATED, NO NEED TO GENERATE INDEXSELECTED
-// 3) DUMB AI
-// 4) SMART AI
-// need to work on indeterminate players; current player is 'one' or 'two'
 
 let playerTurn
 
 const ECKS_CLASS = 'X'
-const CIRCLE_CLASS = 'circle'
-const currentClass = playerTurn ? ECKS_CLASS : CIRCLE_CLASS
+const OH_CLASS = 'O'
+const currentClass = playerTurn ? ECKS_CLASS : OH_CLASS
+
+// const currentClass = if(playerTurn) {ECKS_CLASS} else {OH_CLASS};
 
 function swapTurns() {
   playerTurn = !playerTurn
@@ -29,41 +25,40 @@ const winningTrios = [
 restartBtn.addEventListener('click', startGame);
 
 function startGame() {
+    // document.getElementsByClassName('box').innerHTML = ""
+    // why isn't this clearing the innerhtml?
     stylingOfBoxes.forEach(gridBox => {
-        gridBox.classList.remove('X')
-        gridBox.classList.remove('O')
+        gridBox.classList.remove(ECKS_CLASS)
+        gridBox.classList.remove(OH_CLASS)
         for (const gridBox of stylingOfBoxes) {
             gridBox.addEventListener('click', boxmarked,  {once: true})
         }
     })
 }
+
 const arrayfromBoxes = Array.from(document.getElementsByClassName('box'));
 const stylingOfBoxes = document.querySelectorAll('.box');
 
 function boxmarked(e) {
-    // e.target.innerHTML = "icon goes here";
     const index = arrayfromBoxes.indexOf(e.target)
     
-    if(currentPlayer === 'playerOne') {
-        arrayfromBoxes[index].classList.add('X')
-        currentPlayer = 'playerTwo'
+    if(playerTurn) {
+        arrayfromBoxes[index].classList.add(OH_CLASS)
+        e.target.innerHTML = "O"
       } else {
-        arrayfromBoxes[index].classList.add('O')
-        currentPlayer = 'playerOne'
+        arrayfromBoxes[index].classList.add(ECKS_CLASS)
+        e.target.innerHTML = "X"
       }
 
-    console.log(e.target.classList[1], index, arrayfromBoxes)
+    console.log(currentClass, e.target.classList[1], index, arrayfromBoxes)
+    swapTurns()
     
     checkWin()
 }
 
-// *** FCC does it by passing in board and player. I need the board for the minimax and I need the current player
-// he .reduces the board to get an array of the occupied places
-// then he also employs an .every and for loop (25:00 in video)
-
 function checkWin() {
     var indexSelected = arrayfromBoxes.reduce((indexSelected, box, idx) => {
-        if (box.classList[1] === 'X') {
+        if (box.classList[1] === "X") {
             indexSelected.push(idx);
         }
         return indexSelected;
@@ -77,9 +72,3 @@ function checkWin() {
     // then remove all eventlisteners
 }
 
-// try refactoring to do as below.
-// function checkWin(currentClass) {
-//     return WINNING_COMBINATIONS.some(combination => {
-//       return combination.every(index => {
-//         return cellElements[index].classList.contains(currentClass)
-//       })
