0cdf2a95e45f8d011ae2183c21339effb7bb0f04
Andrew Chang <acchang4@gmail.com>
2021-01-16 13:08:33 -0500

    player switch enabled

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 // To do:
-// make choice by radial buttons
-// make another winning function to highlight and change color (to black?)
-// 1) DUMB AI
+// allow symbol pick then do dumb ai
 // 2) SMART AI
+// 3) animate winning combination
 
-const ECKS_CLASS = 'X'
-const OH_CLASS = 'O'
+// if picked then playerOneSymbol = X (use ternary)
+// instead of ECKS and OH CLASS, use playerOneClass and playerTwoClass
 
+var ONE_CLASS
+var TWO_CLASS 
+
+const btn = document.querySelector('#PlayerOneSymbol');
+btn.onclick = function () {
+    const XOs = document.querySelectorAll('input[name="choice"]');
+    for (const XO of XOs) {
+        if (XO.checked) {
+          ONE_CLASS = XO.value
+          TWO_CLASS = XO.value == 'X' ? 'O' : 'X'
+          break;
+        }
+    }
+    alert(ONE_CLASS);
+    };
+
+let playerTurn
+    
 function swapTurns() {
   playerTurn = !playerTurn
 };
@@ -24,18 +41,19 @@ const winningTrios = [
 
 restartBtn.addEventListener('click', startGame);
 
-let playerTurn
+
 
 function startGame() {
-  swapTurns()
+  console.log(ONE_CLASS, TWO_CLASS)
     stylingOfBoxes.forEach(gridBox => {
-        gridBox.classList.remove(ECKS_CLASS)
-        gridBox.classList.remove(OH_CLASS)
+        gridBox.classList.remove(ONE_CLASS)
+        gridBox.classList.remove(TWO_CLASS)
         gridBox.innerHTML = ""
         for (const gridBox of stylingOfBoxes) {
             gridBox.addEventListener('click', boxmarked,  {once: true})
         }
     })
+  swapTurns()
 }
 
 const arrayfromBoxes = Array.from(document.getElementsByClassName('box'));
@@ -44,11 +62,11 @@ const stylingOfBoxes = document.querySelectorAll('.box');
 function boxmarked(e) {
     const index = arrayfromBoxes.indexOf(e.target)
     if(playerTurn) {
-        arrayfromBoxes[index].classList.add(ECKS_CLASS)
-        e.target.innerHTML = "X"
+        arrayfromBoxes[index].classList.add(ONE_CLASS)
+        e.target.innerHTML = ONE_CLASS
       } else {
-        arrayfromBoxes[index].classList.add(OH_CLASS)
-        e.target.innerHTML = "O"
+        arrayfromBoxes[index].classList.add(TWO_CLASS)
+        e.target.innerHTML = TWO_CLASS
       }
 
     console.log(e.target.classList[1], index, arrayfromBoxes)
@@ -56,7 +74,7 @@ function boxmarked(e) {
     checkWin()
     swapTurns()
 
-    // if radial checked then play dumbAI
+    // if radial checked, shut off receiver then play dumbAI
     // make an array of where e.target.innerHTML = ""
     // then pick one of the spaces
     // arrayfromBoxes[dumbpick].classList.add(ECKS_CLASS)
@@ -66,9 +84,9 @@ function boxmarked(e) {
 
 function checkClass() {
   if(playerTurn) {
-  return ECKS_CLASS
+  return ONE_CLASS
 } else {
-  return OH_CLASS
+  return TWO_CLASS
 };}
 
 function checkWin() {
