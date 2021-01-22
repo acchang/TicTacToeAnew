7e80efea689d3764ea321ee4569ac2d09793c600
Andrew Chang <acchang4@gmail.com>
2021-01-21 21:21:12 -0500

    added endGame so no added move

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-// pre-empt the opponent's last move -- by making playerdesignation inactive
+// 1) pre-empt the opponent's last move -- by making playerdesignation inactive (works but ugly and not explain)
   // I would try to make the boxmarked function behave different based on what checkwin() returns.
+  // break it up
 
-  // identify winning trio to add class and change background color
+  // think about it, what should checkWin() do? Maybe even think of a better name, I might even call it something like userHasWon(), and 
+  // think of how nice it will be to read if(userHasWon()) you can tell right away what is happening.
+  // add condition to boxmarked if not won and AI
+
+
+  // 2) find a way to slow the response
+
+  // 3) identify winning trio to add class and change background color
   // if (isWinningCombo) {
   //   winner = true;
   //   winningCombo.forEach((index) => {
   //     positions[index].className += ' winner';}
   //   }
 
-  // need to denote a tie, I guess when all spaces are filled, place after checkwin
+  // 4) need to denote a tie, I guess when all spaces are filled, place after checkwin
+  
   // then smart AI
 
-  
+
 
 var ONE_CLASS
-var TWO_CLASS 
+var TWO_CLASS
+var endGame 
 
 const btn = document.querySelector('#PlayerOneSymbol');
 
@@ -68,6 +78,7 @@ function startGame() {
   console.log("player 1 = " + ONE_CLASS + ", player 2 = " + playerTwoIdentity)
   drawBoard();
   playerTurn = true
+  endGame = false
 }
 
 const arrayfromBoxes = Array.from(document.getElementsByClassName('box'));
@@ -102,19 +113,24 @@ function boxmarked(e) {
     checkWin()
     swapTurns()
 
-    if(playerTwoIdentity === "Dumb AI") {
+    if(playerTwoIdentity === "Dumb AI" && endGame === false) {
       var dumbAIArray = arrayfromBoxes.reduce((dumbAIArray, box, idx) => {
         if (box.innerHTML === "") {
           dumbAIArray.push(idx);
           }
           return dumbAIArray;
         }, []);
-        console.log("player 2 picks from " + dumbAIArray);
+        console.log(endGame + " player 2 picks from " + dumbAIArray);
         let dumbAIpicked = dumbAIArray[Math.floor(dumbAIArray.length * (Math.random()))]
         console.log("player 2 picks " + dumbAIpicked);
         arrayfromBoxes[dumbAIpicked].classList.add(TWO_CLASS)
         arrayfromBoxes[dumbAIpicked].innerHTML = TWO_CLASS
 
+// why does Timeoutfunction prevent "O wins"? this is 100% responsible
+// setTimeout(function(){arrayfromBoxes[dumbAIpicked].classList.add(TWO_CLASS)}, 500);
+// setTimeout(function(){arrayfromBoxes[dumbAIpicked].innerHTML = TWO_CLASS}, 500);
+// I could break off opponent move and mayb slow it?
+    
     checkWin()
     swapTurns()
     } else { console.log("Human")
@@ -140,7 +156,10 @@ function checkWin() {
    var winner = winningTrios.some(trio => {
        return trio.every(i => indexOfSelected.includes(i))});
 
-    if (winner === true) {declareWinner()};
+    if (winner === true) {
+      endGame = true
+      declareWinner()};
+    // I could add a 'let' here so checkwin would return something but boxmarked would still go
     console.log("all spaces by player " + checkClass() + " is " + indexOfSelected);
 }
 
